564b4794a936290c24492a23813b3751730f8fdb
Boris Matsakov <105161004+Necentt@users.noreply.github.com>
2023-10-16 19:02:57 +0300

    Update why_anomalies.py

--- discovery/tools/pyod/why_anomalies.py
+++ discovery/tools/pyod/why_anomalies.py
@@ -1,30 +1,28 @@
-import requests
-import json
+import os
+import pandas as pd
+import openai
+from json import loads, dumps
+from dotenv import load_dotenv
 
 
-base_url = "https://api.openai.com/v1/chat/completions"
-api_key = "YOUR_API_KEY"
-headers = {
-    "Authorization": f"Bearer {api_key}",
-    "Content-Type": "application/json"
-}
-anomaly_str = open('predicted_anomalies.csv').readlines()
-anomaly_str = f'\n'.join(anomaly_str)
-data = {
-    "model": "gpt-3.5-turbo",
-    "messages": [
-        {"role": "system", "content": "You're a pro at finding anomalies in a basketball game."},
-        {"role": "user", "content": f"This data is anomalous, that is, it stands out from other data. Tell me, why can they be anomaly? {anomaly_str}"}
-    ]
-}
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
-response = requests.post(base_url, headers=headers, data=json.dumps(data))
-if response.status_code == 200:
-    result = response.json()
-    generated_text = result['choices'][0]['message']['content']
-    my_file = open('why_anomalies_gpt.txt', 'a+')
-    my_file.write(f'\n\n{generated_text}')
-    my_file.close()
-    print(generated_text)
-else:
-    print("Ошибка при запросе к API:", response.status_code)
+df = pd.read_csv('predicted_anomalies.csv')
+result = df.to_json(orient="split")
+parsed = loads(result)
+
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+      {"role": "system", "content": "You're a pro at finding anomalies in a basketball game."},
+      {"role": "user", "content": f"This data is anomalous, that is, it stands out from other data. Tell me, why "
+                                  f"can they be anomaly? {dumps(parsed, indent=4)}"}
+  ]
+)
+
+
+my_file = open('why_anomalies_gpt.txt', 'a+')
+my_file.write(f'\n\n{completion.choices[0].message.content}')
+my_file.close()
+print(completion.choices[0].message.content)
