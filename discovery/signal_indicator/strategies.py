96563b5b17ec9c41138a07ad0d727e3e5b9b014e
Bogdan <61659864+Bogdalian@users.noreply.github.com>
2023-11-20 11:26:18 +0300

    Update strategies.py

--- discovery/signal_indicator/strategies.py
+++ discovery/signal_indicator/strategies.py
@@ -95,14 +95,9 @@ class StrategyRating(Strategy):
         new_rating_N = {indicator: value.iloc[:top] for indicator, value in new_rating.items()}
         
         old_top = {col: set(old_rating_N[col]['PLAYER_ID'].to_list()) for col in old_rating_N.keys()}
-        
-        if new_rating_N:
-            new_rating_N = {indicator: value.iloc[:top] for indicator, value in self.new_rating.items()}
-            new_top = {col: set(new_rating_N [col]['PLAYER_ID'].to_list()) for col in new_rating_N.keys()}
-        else:
-           new_top = {col: set(new_rating_N [col]['PLAYER_ID'].to_list()) for col in new_rating_N.keys()} 
+        new_top = {col: set(new_rating_N [col]['PLAYER_ID'].to_list()) for col in new_rating_N.keys()}
 
-       
+
         for col in self.calculus_columns:
                 in_top =  new_top[col] - old_top[col]
                 out_top = old_top[col] -  new_top[col]
@@ -115,4 +110,5 @@ class StrategyRating(Strategy):
         answer = {'description': f'Top {top} players',
                   'values': result}                
         return answer
+    
     
