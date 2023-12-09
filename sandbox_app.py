104e4d6737b37d6fb5deabc3d538f241bbde2e67
no-one2k <no-one2k@yandex.ru>
2023-12-09 14:33:51 +0400

    fixed date selection and re-requesting nba_api issues

--- sandbox_app.py
+++ sandbox_app.py
@@ -13,7 +13,7 @@ SQLITE_DB_PATH = "test.db"
 
 
 @flow
-def get_today_ids() -> List[str]:
+def get_game_ids_for_date(date: dt.date) -> List[str]:
     """
     Finds id's of nba games that have already ended
 
@@ -21,9 +21,13 @@ def get_today_ids() -> List[str]:
         list
             string id
     """
-    current_date = dt.datetime.today() - dt.timedelta(days=1)
-    current_date = current_date.strftime('%m/%d/%Y')
-    games = leaguegamefinder.LeagueGameFinder(date_from_nullable=current_date).get_data_frames()[0]
+    to_date_str = (date + dt.timedelta(days=1)).strftime('%m/%d/%Y')
+    from_date_str = date.strftime('%m/%d/%Y')
+    print(from_date_str, to_date_str)
+    games = leaguegamefinder.LeagueGameFinder(
+        date_from_nullable=from_date_str,
+        date_to_nullable=to_date_str,
+    ).get_data_frames()[0]
     games_id = list()
     for i in games['GAME_ID']:
         if i not in games_id and i[0] == '0':
@@ -31,10 +35,8 @@ def get_today_ids() -> List[str]:
     return games_id
 
 
-
 def check_password():
     """Returns `True` if the user had the correct password."""
-
     def password_entered():
         """Checks whether a password entered by the user is correct."""
         if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
@@ -86,6 +88,31 @@ def select_missing_game_ids(game_ids: List[str], db_path: str) -> (pd.DataFrame,
     return selected_games_df, missing_games_df
 
 
+def on_get_games():
+    _date = st.session_state['date']
+    games = get_game_ids_for_date(_date)
+    st.session_state["run_for_date"] = _date
+    st.session_state["game_id_options"] = games
+    st.session_state["game_id_default"] = games
+
+
+def on_collect():
+    _game_id = st.session_state["select_game_id"]
+    if _game_id:
+        selected_games_df, missing_games_df = select_missing_game_ids(_game_id, SQLITE_DB_PATH)
+        st.session_state.selected_games_df = selected_games_df
+        st.session_state.txt_tweets_from_db = "\n".join(f"\t\ttweet based on {g}" for g in selected_games_df.game_id)
+        missing_games_df['game_id'] = missing_games_df.index.map(int)
+        missing_games_df['game_date'] = st.session_state["run_for_date"]
+        missing_games_df['teams_slug'] = missing_games_df.index
+        with sqlite3.connect(SQLITE_DB_PATH) as con:
+            missing_games_df.to_sql(con=con, name='games', if_exists='append', index=False)
+        st.session_state.missing_games_df = missing_games_df
+        st.session_state.txt_new_tweets = "\n".join(f"\t\ttweet based on {g}" for g in missing_games_df.game_id)
+    else:
+        st.info("no games to collect")
+
+
 def add_app_logic():
     st.title("Basnya Prototype")
 
@@ -93,44 +120,33 @@ def add_app_logic():
 
     _date = st.date_input(
         label="enter date",
-        value=dt.date.today(),
+        value=dt.date.today() - dt.timedelta(days=1),
         min_value=dt.date(2020, 1, 1),
         max_value=dt.date.today(),
-        format='DD.MM.YYYY'
+        format='DD.MM.YYYY',
+        key="date"
     )
 
-    button1 = st.button("Get games")
-    if st.session_state.get('button') != True:
-        st.session_state['button'] = button1
-    if st.session_state['button'] == True:
-        games = get_today_ids()
-        st.write(f"Games for {_date}: {','.join(games)}")
-
-        st.header("Collect data for game")
-
-        _game_id = st.multiselect(
-            label="select games",
-            options=games,
-            default=games,
-        )
-        if st.button("Collect"):
-            if _game_id:
-                selected_games_df, missing_games_df = select_missing_game_ids(_game_id, SQLITE_DB_PATH)
-                st.subheader("Processed games:")
-                st.dataframe(selected_games_df, hide_index=True)
-                for g in selected_games_df.game_id:
-                    st.write(f"\t\ttweet based on {g}")
-                missing_games_df['game_id'] = missing_games_df.index.map(int)
-                missing_games_df['game_date'] = _date
-                missing_games_df['teams_slug'] = missing_games_df.index
-                with sqlite3.connect(SQLITE_DB_PATH) as con:
-                    missing_games_df.to_sql(con=con, name='games', if_exists='append', index=False)
-                st.subheader("New games:")
-                st.dataframe(missing_games_df, hide_index=True)
-                for g in missing_games_df.game_id:
-                    st.write(f"\t\ttweet based on {g}")
-            else:
-                st.info("no games to collect")
+    st.button("Get games", key='btn_get_games', on_click=on_get_games)
+    st.header("Collect data for game")
+    if "game_id_options" not in st.session_state:
+        st.session_state.game_id_options = []
+        st.session_state.game_id_default = []
+        st.session_state.selected_games_df = pd.DataFrame()
+        st.session_state.missing_games_df = pd.DataFrame()
+    _game_id = st.multiselect(
+        label="select games",
+        options=st.session_state.game_id_options,
+        default=st.session_state.game_id_default,
+        key='select_game_id'
+    )
+    st.button("Collect", key='btn_collect', on_click=on_collect)
+    st.subheader("Processed games:")
+    st.dataframe(data=st.session_state.selected_games_df, hide_index=True)
+    st.text_area("", key="txt_tweets_from_db")
+    st.subheader("New games:")
+    st.dataframe(data=st.session_state.missing_games_df, hide_index=True)
+    st.text_area("", key="txt_new_tweets")
 
 
 def main():
